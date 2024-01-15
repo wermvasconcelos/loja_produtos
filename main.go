72a45083b296e0db3cabfca897e3dc1487f80cd8
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-15 16:54:49 -0300

    feat: serve and bootstramp in html

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "Index", nil)
 }
