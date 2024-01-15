4b5879544a67fe16d34696ea0d4c80c88a1b1709
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-15 17:42:54 -0300

    feat: connection postgres sql

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=39151626dim host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
 
 type Produto struct {
 	Nome, Descricao string
