d5138d24b61ccc3cdc043681b7b6ae5a28e56682
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-17 16:42:28 -0300

    feat: insert produto

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"log"
 	"loja_produtos/models"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -15,4 +17,27 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func New(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço: ", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade: ", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
