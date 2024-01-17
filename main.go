05bb8b25d45d3ac1dc7e607ac328f4135b783233
wermvasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-17 15:37:39 -0300

    refactor: modularização

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
+	"loja_produtos/models"
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
-
-func conectaComBancoDeDados() *sql.DB {
-	conexao := "user=postgres dbname=alura_loja password=123456 host=localhost sslmode=disable" //porta 5432
-	db, err := sql.Open("postgres", conexao)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
-}
-
-type Produto struct {
-	Nome, Descricao string
-	Preco           float64
-	Quantidade, id  int
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -33,35 +14,6 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db := conectaComBancoDeDados()
-	defer db.Close()
-
-	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &quantidade, &preco)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Quantidade = quantidade
-		p.Preco = preco
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 }
---
+++ models/produtos.go
@@ -0,0 +1,45 @@
+package models
+
+import (
+	"loja_produtos/db"
+)
+
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	Quantidade, id  int
+}
+
+func BuscaTodosOsProdutos() []Produto {
+	db := db.ConectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &quantidade, &preco)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Quantidade = quantidade
+		p.Preco = preco
+
+		produtos = append(produtos, p)
+	}
+
+	defer db.Close()
+	return produtos
+}
